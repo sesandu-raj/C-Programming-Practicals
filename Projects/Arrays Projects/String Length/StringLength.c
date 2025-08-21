53d2697a806c6187013a84dca6d2ea34b12fae06
Sesandu <sesanduj@gmail.com>
2025-08-21 11:14:55 +0530

    Finding string Length

--- Projects/Arrays Projects/String Length/StringLength.c
+++ Projects/Arrays Projects/String Length/StringLength.c
@@ -3,3 +3,23 @@
 int main (void) {
     
 }
+
+char *get_string(const char *prompt) {
+    printf("%s", prompt);
+    int capacity = 16, length = 0;
+    char *buffer = malloc(capacity);
+    if (!buffer) return NULL;
+
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        if (length >= capacity - 1) {
+            capacity *= 2;
+            char *temp = realloc(buffer, capacity);
+            if (!temp) { free(buffer); return NULL; }
+            buffer = temp;
+        }
+        buffer[length++] = (char)c;
+    }
+    buffer[length] = '\0';
+    return buffer;
+}
