13092cbfc1c3836e676c9e96ab8e1eff671b955e
Sesandu <sesanduj@gmail.com>
2025-08-22 20:08:04 +0530

    2D Array Created and store Data in Arrays

--- Arrays/2DArray.c
+++ Arrays/2DArray.c
@@ -7,16 +7,19 @@ int main (void){
     scanf("%d", &no_of_students);
 
     char names[no_of_students][50];
+    int name_lengths[no_of_students];
 
     for(int i = 0; i < no_of_students; i++) {
         printf("Enter name of student %d: ", i + 1);
         scanf("%49s", names[i]);
         int length = strlen(names[i]);
+        name_lengths[i] = length;
+        printf("Length of name %s: %d\n",names[i], name_lengths[i]);
     }
 
     printf("Names of students:\n");
     for(int i = 0; i < no_of_students; i++) {
-        printf("%s\n", &names[i]);
+        printf("%s\n", names[i]);
     }
 
     printf("%c, %c, %c\n", names[0][0], names[0][1], names[0][2]);
