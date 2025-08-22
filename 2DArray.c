6db0005d1941c169004f4fe9bb994dc40c2eb714
R34P3r <sesanduj@outlook.com>
2025-08-22 14:50:27 +0530

    Create 2DArray.c
    Takes students names to array and store that names in another array. It's 2D array.

---
+++ 2DArray.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int main (void){
+    int no_of_students;
+    printf("Enter the number of students: ");
+    scanf("%d", &no_of_students);
+
+    char names[no_of_students][50];
+
+    for(int i = 0; i < no_of_students; i++) {
+        printf("Enter name of student %d: ", i + 1);
+        scanf("%49s", names[i]);
+        int length = strlen(names[i]);
+    }
+
+    printf("Names of students:\n");
+    for(int i = 0; i < no_of_students; i++) {
+        printf("%s\n", &names[i]);
+    }
+
+    printf("%c, %c, %c\n", names[0][0], names[0][1], names[0][2]);
+    printf("%c, %c, %c\n", names[1][0], names[1][1], names[1][2]);
+    printf("%c, %c, %c\n", names[2][0], names[2][1], names[2][2]);  
+
+    return 0;
+
+}
