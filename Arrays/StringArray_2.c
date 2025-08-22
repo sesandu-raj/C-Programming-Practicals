accf6b65d8d23981ac591cb05ab14c925b20af29
Sesandu <sesanduj@gmail.com>
2025-08-22 22:19:47 +0530

    StringArray_2.c is for Declaring array and enter data using scanf

--- Arrays/StringArray_2.c
+++ Arrays/StringArray_2.c
@@ -8,7 +8,16 @@ int main (void){
     printf("Your string is: %s\n", name);
 
     for(int i=0; i<strlen(name); i++){
-        printf("%c\n", name[i]);
+        printf("%c %i\n", name[i], name[i]);
     }
+    printf("\n");
+
+    char secret[100]; // Allocate space for the string, No Dynamic Memory Allocation
+    for(int i=0; i<strlen(name); i++){
+        secret[i] = name[i] + 1; // Increment each character by 1
+        printf("%c %i\n", secret[i], secret[i]);
+    }
+    secret[strlen(name)] = '\0'; // Null-terminate the secret string
+    printf("Your secret string is: %s\n", secret);
     return 0;
 }
